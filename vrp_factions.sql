03cb723b9456aec56c28e58d3848912c7571a8e9
SekinM <70114080+SekinM@users.noreply.github.com>
2021-04-13 23:54:03 +0200

    Fixed menu showing up when not in a faction

--- vrp_factions.sql
+++ vrp_factions.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `vrp_users` ADD (
-  `faction` varchar(128) DEFAULT NULL,
-  `factionRank` varchar(128) DEFAULT NULL,
+  `faction` varchar(128) NULL DEFAULT 'user',
+  `factionRank` varchar(128) NULL DEFAULT 'none',
   `isFactionLeader` tinyint NOT NULL DEFAULT '0',
   `isFactionCoLeader` int NOT NULL DEFAULT '0'
 );
